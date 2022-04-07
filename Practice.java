9e80556bdf87ddf5d539e8be1778678d722f376f
Karthiga Jayaperumal <99970828+KarthigaJayaperumal@users.noreply.github.com>
2022-04-07 19:37:55 +0530

    Practice Program
    Practice Program

---
+++ Practice.java
@@ -0,0 +1,25 @@
+package april_program;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		for(int row=1;row<=7;row++) {
+			for(int col=1;col<=7;col++) {
+			if(row==1||row==7||col==1||col==7||row==4)	
+			{
+			System.out.print("* ");	
+				
+			}
+			else {
+				
+			System.out.print("  ");	
+				
+			}
+		
+			
+		}
+			System.out.println();
+			}
+		}
+}
+		
